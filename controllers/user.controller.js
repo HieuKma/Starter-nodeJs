6bd8a6d068e1216a2c9398976124b9e07d111566
hieuitkma <hieuitkma@gmail.com>
2020-07-20 16:28:23 +0700

    Add middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -32,20 +32,9 @@ module.exports.getId = (req, res) => {
 
 module.exports.postCreate = (req, res) => {
     req.body.id = shortid.generate();
-    let errors = [];
-    if(!req.body.name) {
-        errors.push('Name is required !');
-    }
-    if(!req.body.phone) {
-        errors.push('Phone is required !');
-    }
-    if(errors.length) {
-        res.render('users/create', {
-            errors: errors,
-            values: req.body
-        });
-        return;
-    }
+
+    console.log(res.locals);
+
     db.get('users').push(req.body).write();
     res.redirect('/users');
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,21 @@
 var express = require('express');
 const controller = require('../controllers/user.controller');
+// Muốn đi từ A->B thì phải đi là A->C->D->E->F->B thì (C->D->E->F) là middleware(có thể có hoặc không
+const validate = require('../validate/user.validate');
 
 var router = express.Router();
 
 router.get('/', controller.index);
+
+// function middleware1(req, res, next) {
+//     console.log('Middleware 1');
+//     next(); // chuyen sang middleware 2
+// }
+// function middleware2(req, res, next) {
+//     console.log('Middleware 2');
+//     res.send('Hello');
+// }
+// router.get('/test', middleware1, middleware2);
 
 router.get('/search', controller.search);
 
@@ -11,6 +23,8 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.getId);
 
-router.post('/create', controller.postCreate);
+// Kiem tra validate, neu co loi thi nhap lai
+// Neu khong co loi thi => controller.postCreate
+router.post('/create', validate.postCreate, controller.postCreate);
 
 module.exports = router;
